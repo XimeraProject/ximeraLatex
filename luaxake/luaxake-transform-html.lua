d622781791e571cbff4ca931364ea84e42c8e847
Wim Obbels <wiobber@users.noreply.github.com>
2025-06-10 20:57:28 +0200

    localization/outcomes  (from luaxakeuf) (#174)
    * backup more files.
    
    * added more dtx files.
    
    * Backup
    
    * Added 'morewrites' package to fix silly tex core issues.
    
    * Updated the footnote source to correctly color and format in-line footnotes.
    
    * Updated a variety of files to improve the author and outcome handling to allow for footnotes to be used. Also added better support for footnotes themselves, both in pdf and when published online. Colored footnotes are now working.
    
    * Fixed the title.dtx in src to have correct conditionals.
    
    * Backup
    
    * trying to sync back to osu for extra branch stuff.
    
    * Trying to make this work.
    
    * Forcing a commit.
    
    * Fixed a typo on author and outcome conditionals.
    
    * New features added but we need to merge back to osu/master branch.
    
    * Making sure everything is updated.
    
    * Added outcomes and package updates to use new outcomes mechanisms
    
    * Trying to remerge - step one.
    
    * Fixed the ximera.ins file.
    
    * Added needed lines to ximera.dtx. May need to add more to various configs once I can test.
    
    * ximera.dtx: fix order (?)
    
    * 'make'
    
    * Update title.dtx
    
    Commented out the 'theoutcomes' command as it is depreciated with new outcome mechanism.
    
    * Update ximera.dtx
    
    Fixed the load order of outcome.dtx to come after only.dtx for conditionals.
    
    * Update ximera.ins
    
    Fixed the load order of outcome.dtx to come after only.dtx for conditionals.
    
    * --platform does not work
    
    * Update outcomes.dtx
    
    replaced with real outcomes.dtx
    
    * add devcontainer (from firstSteps)
    
    * Update packages.dtx
    
    added currfile package
    
    * Update packages.dtx
    
    oops, wrong syntax.
    
    * Update sameDerivative.dtx
    
    Missing a closed paren.
    
    * actually built the new cls file. I'll get it right one of these days.
    
    * remove obsolete files
    
    To be verified ...?
    
    * restore jax.dtx from master
    
    * problem.dtx: only needed changes (hopefully...)
    
    * Dockerfile: add adjustbox (for trimclip in validators)
    
    * Dockerfile: add lipsum
    
    * Cleaned up some of the validator code - still needs to be fixed, so it is still not loaded into the main dtx.
    
    * Removed the validator content from the build while an exotic bug is hunted down and squashed. Also did some minor reordering to fix a minor clash issue.
    
    * Added conditionals to only load the localization features if babel was loaded by the user in the preamble. To ensure thorough detection of package loading, the conditional is executed using the built in hook to place the loading of the localization content right before the begin{document} command is executed.
    
    * Removed localization package requirements from the default load, and moved them over to the conditional load.
    
    * Removed old Optional Files folder, and commented out the .oc file extension from the autoclean list on luaxake, since the .oc files are now used for the outcomes revamp.
    
    * Fixed some spacing and addressed some comments from Bart.
    
    * Forgot to actually run make. Oops.
    
    * Update problem.dtx
    
    Didn't have the translation bit correct.
    
    * Fixed the problem.dtx and then ran make to build the cls and supplement files.
    
    * dutch: typo's
    
    * Update outcomes.dtx
    
    Fixed "Output" to "Outcome" as was originally intended, for the name of a few hook macros.
    
    * Update localize.dtx
    
    Don't need to load other language files - although we should load the babel language, so I'll figure this out later.
    
    * First stab at localization dynamically loading augment files.
    
    * Not sure why it needs another immediate commit?
    
    * Fixed a minor type and tested - seems to work.
    
    * Fixed localization.dtx to eliminate the uppercase issue.
    
    * rebuilt ximera.cls with changes.
    
    * luaxake: remove babel newcommands with '?' or digit
    
    * Fixed capitalization when babel is not loaded.
    
    ---------
    
    Co-authored-by: XronosUF-Jason <XronosUF@GMail.com>
    Co-authored-by: JasonN <jnowell@ufl.edu>
    Co-authored-by: Wim Obbels <wim.obbels@kuleuven.be>

--- luaxake/luaxake-transform-html.lua
+++ luaxake/luaxake-transform-html.lua
@@ -536,6 +536,8 @@ local function post_process_html(cmd)
     filtered_cmds= filtered_cmds:gsub("[^\n]*[:*@].-\n", "")      -- remove all 'exotic' characters; _ must be kept...
     filtered_cmds= filtered_cmds:gsub("[^\n]\\_.-\n", "")          -- remove \_  (Mathax error)
     filtered_cmds= filtered_cmds:gsub("[^\n]\\TU.-\n", "")          -- remove \_  (Mathax error)
+    filtered_cmds= filtered_cmds:gsub("[^\n]\\T1.-\n", "")         -- remove \T1  (Mathax error, babel)
+    filtered_cmds= filtered_cmds:gsub("[^\n]\\%?.-\n", "")         -- remove \?   (Mathax error, babel)
     filtered_cmds= filtered_cmds:gsub("[^\n]\\label.-\n", "")      -- remove \label  (Mathax error)
     filtered_cmds= filter_newcommands(filtered_cmds)               -- only keep newcommands and declaremathoperator
     filtered_cmds= filtered_cmds:gsub("##(%d)", "#%1")             -- replace ##1 with #1
